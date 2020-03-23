436341c3f24225efcfad359f0f11ec73eef1c44b
FranPaya19911 <paya19911@iesmarenostrum.com>
2020-03-23 10:59:33 +0100

    cambio

---
+++ aplicacion/aplicacion/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace aplicacion
+{
+    public class Class1
+    {
+        
+        static void Main()
+        {
+            Console.WriteLine("hola");
+        }
+    }
+}
